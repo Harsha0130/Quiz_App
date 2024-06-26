4af5ec40793fe239b67e8bb114491ee77b9add8a
Harsha <harshahm0130@gmail.com>
2024-06-26 22:48:35 +0530

    Adding questions to canvas

--- ui.py
+++ ui.py
@@ -1,33 +1,41 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizz")
         self.window.config(pady=20, padx=20, bg=THEME_COLOR)
 
-        self.score = Label(text="Score: 0", bg=THEME_COLOR, fg="white", font=("Arial", 10, "bold"))
+        self.score = Label(text="Score: 0", bg=THEME_COLOR, fg="white", font=("Arial", 15, "bold"))
         self.score.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question_txt = self.canvas.create_text(
             150,
             125,
-            text="Questions appears here",
+            width=280,
+            text="",
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
         )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
-        self.true_btn = Button(image=true_image, highlightthickness=0, bg=THEME_COLOR)
+        self.true_btn = Button(image=true_image, highlightthickness=0)
         self.true_btn.grid(row=2, column=0)
 
         false_image = PhotoImage(file="images/false.png")
-        self.false_btn = Button(image=false_image, highlightthickness=0, bg=THEME_COLOR)
+        self.false_btn = Button(image=false_image, highlightthickness=0)
         self.false_btn.grid(row=2, column=1)
 
+        self.get_nxt_question()
         self.window.mainloop()
+
+    def get_nxt_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_txt, text=q_text)
